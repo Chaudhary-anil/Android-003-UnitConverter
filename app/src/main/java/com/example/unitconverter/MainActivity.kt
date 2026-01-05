0151d0d63294618459638dd15861ee17bdc33495
Anil <chaudharyanil5656@gmail.com>
2026-01-05 21:55:30 +0545

    Initial project setup

---
+++ app/src/main/java/com/example/unitconverter/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.example.unitconverter
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import com.example.unitconverter.ui.theme.UnitConverterTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            UnitConverterTheme {
+
+            }
+        }
+    }
+}
